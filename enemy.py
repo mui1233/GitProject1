a0835969fc5bad2ac0b9d127253f1d31cc8ecdea
mui1233 <145476141+mui1233@users.noreply.github.com>
2024-05-28 11:06:08 -0400

    Created enemy sprite and worked on how to make it not blit after projectile collision.

---
+++ enemy.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Enemy:
+
+    def __init__(self, x, y):
+
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("blue-fox-sprite.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = .1
+        self.current_direction = "right"
+        self.health = 100
---
+++ main.py
@@ -0,0 +1,106 @@
+import pygame
+from fox import Fox
+from projectile import Projectile
+from enemy import Enemy
+import random
+# set up pygame modules
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 15)
+pygame.display.set_caption("Coin Collector!")
+
+# set up variables for the display
+SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 900
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+
+message = "Collision not detected"
+r = 50
+g = 0
+b = 100
+# render the text for later
+display_message = my_font.render(message, True, (255, 255, 255))
+
+f = Fox(40, 60)
+score = 0
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
+run = True
+start_game = False
+blit_proj = False
+projectiles = []
+right = False
+directions = []
+health = 100
+alive = True
+# -------- Main Program Loop -----------
+while run:
+
+
+    if start_game:
+        keys = pygame.key.get_pressed()  # checking pressed keys
+        if keys[pygame.K_d]:
+            f.move_direction("right")
+        if keys[pygame.K_a]:
+            f.move_direction("left")
+
+        if keys[pygame.K_w]:
+            f.move_direction("up")
+
+        if keys[pygame.K_s]:
+            f.move_direction("down")
+
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+        if event.type == pygame.MOUSEBUTTONUP and start_game == False:
+            start_game = True
+            message = "Click to play!"
+            message_display = my_font.render(message, True, (255, 255, 255))
+        if start_game == True:
+            if event.type == pygame.MOUSEBUTTONUP:
+                blit_proj = True
+                proj = Projectile(f.x, f.y)
+                projectiles.append(proj)
+                if f.current_direction == 'right':
+                    right = True
+                    directions.append(right)
+                elif f.current_direction == 'left':
+                    right = False
+                    directions.append(right)
+                print(projectiles)
+                e = Enemy(400, 400)
+                if pygame.Rect.colliderect(e, proj):
+                    alive = False
+
+    if start_game:
+        score_display = my_font.render(f"Points: {score}", True, (255, 255, 255))
+        screen.fill((r, g, b))
+        screen.blit(score_display, (0, 30))
+
+        screen.blit(display_message, (0, 15))
+        screen.blit(f.image, f.rect)
+        if alive:
+            screen.blit(e.image, e.rect)
+
+        if blit_proj:
+            for p in projectiles:
+                screen.blit(p.image, p.rect)
+            for p in projectiles:
+                if directions[projectiles.index(p)]:
+                    p.move_right()
+                else:
+                    p.move_left()
+
+        pygame.display.update()
+    else:
+        screen.fill((r, g, b))
+        screen.blit(display_message, (200, 200))
+        pygame.display.update()
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+
+
+
